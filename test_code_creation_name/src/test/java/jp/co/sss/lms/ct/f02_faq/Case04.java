a4ec8549de1ffd838b0e7d1263acb49f1af48f55
user <user@WIN-NDAPNIHP71N>
2026-09-24 13:35:40 +0900

    fix: Case04 深井 test02メソッドの編集
    Case03から引用

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト よくある質問機能
@@ -45,7 +47,21 @@ public class Case04 {
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//DBにないIDとパスワードを入力
+        webDriver.findElement(By.name("loginId")).sendKeys("StudentAA03");
+        webDriver.findElement(By.name("password")).sendKeys("StudentAA03");
+        //ログインボタンをクリック
+        webDriver.findElement(By.cssSelector("input[type='submit']")).click();
+        //コース詳細にある詳細ボタン表示までの時間確保
+        visibilityTimeout(By.cssSelector("input[value='詳細']"), 5);
+        //タイトルエビデンス取得
+      	String evidenceTitle = "コース詳細 | LMS";
+      	assertEquals(evidenceTitle, webDriver.getTitle());
+      	//各セクション詳細ボタン表示エビデンス取得
+      	boolean evidenceButton = webDriver.findElement(By.cssSelector("input[value='詳細']")).isDisplayed();
+      	assertTrue(evidenceButton);
+        //エビデンス証跡取得
+        getEvidence(new Object(){}, "テスト02");
 	}
 
 	@Test
