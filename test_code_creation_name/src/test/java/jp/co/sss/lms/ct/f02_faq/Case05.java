7fd7241e3fac15cbdc31a1ee94c916bc9e96453e
user <user@WIN-NDAPNIHP71N>
2026-09-24 16:31:43 +0900

    fix:Case05 深井 test03,04メソッドの修正
    Case04を引用しtest03,04メソッドを修正しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -3,6 +3,8 @@ package jp.co.sss.lms.ct.f02_faq;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -83,14 +85,56 @@ public class Case05 {
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//ヘッダーにある「機能」をクリック
+		webDriver.findElement(By.linkText("機能")).click();
+				
+		//「機能」にある「ヘルプ」が表示させるまでの時間確保
+		visibilityTimeout(By.linkText("ヘルプ"), 5);
+				
+		//「機能」にある「ヘルプ」をクリック
+		webDriver.findElement(By.linkText("ヘルプ")).click();
+				
+		//タイトルエビデンス取得
+		String evidenceTitle = "ヘルプ | LMS";
+		assertEquals(evidenceTitle, webDriver.getTitle());
+				
+		//見出しエビデンス取得
+		String evidenceHeading = "ヘルプ";
+		String actualHeading = webDriver.findElement(By.tagName("h2")).getText();
+		assertEquals(evidenceHeading, actualHeading);
+				
+		//エビデンス証跡取得
+		getEvidence(new Object(){}, "テスト03");
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		//「よくある質問」リンクをクリック
+		webDriver.findElement(By.linkText("よくある質問")).click();
+				
+		//クリック前のタブIDを取得
+		String originalWindow = webDriver.getWindowHandle();
+	    // ブラウザで開いているすべてのタブID一覧を取得し、元のタブを削除
+		Set<String> handles = webDriver.getWindowHandles();
+		handles.remove(originalWindow);
+		//新しいタブへ操作を切り替える
+	    webDriver.switchTo().window(handles.iterator().next());
+			    
+		//よくある質問にある「検索」ボタン表示させるまでの時間確保
+		visibilityTimeout(By.cssSelector("input[value='検索']"), 5);
+				
+		//タイトルエビデンス取得
+		String evidenceTitle = "よくある質問 | LMS";
+		assertEquals(evidenceTitle, webDriver.getTitle());
+				
+		//検索ボタン表示エビデンス取得
+		boolean evidenceButton = webDriver.findElement(By.cssSelector("input[value='検索']")).isDisplayed();
+		assertTrue(evidenceButton);
+		      	
+		//エビデンス証跡取得
+		getEvidence(new Object(){}, "テスト04");
 	}
 	@Test
 	@Order(5)
