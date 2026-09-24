22c5ea9a6b87fc94889c334c843a0005596f30a7
user <user@WIN-NDAPNIHP71N>
2026-09-24 16:24:12 +0900

    fix:Case05 深井 test01メソッドの修正
    Case04から引用しtest01メソッドを修正しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -38,7 +38,19 @@ public class Case05 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページへアクセス
+		goTo("http://localhost:8080/lms/");
+				
+		//タイトルエビデンス取得
+		String evidenceTitle = "ログイン | LMS";
+		assertEquals(evidenceTitle, webDriver.getTitle());
+				
+		//ボタン表示エビデンス取得
+		boolean evidenceButton = webDriver.findElement(By.cssSelector("input[type='submit']")).isDisplayed();
+		assertTrue(evidenceButton);
+				
+		//エビデンス証跡取得
+		getEvidence(new Object(){}, "テスト01");
 	}
 
 	@Test
